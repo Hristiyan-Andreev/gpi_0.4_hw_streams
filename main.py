a22cd33805890f57f1cfe495d3bdfc68e44a2d9d
Hristiyan Andreev <hrvalandreev@gmail.com>
2018-12-05 11:14:44 +0000

    Config file taken away from the project

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import sys
 import liveapi
 import time
 import RPi.GPIO as GPIO
+
+from flask import Flask
+
+sys.path.append('/home/pi/config')
 import config as cf
-
-
 
 # Time measurement class
 class TimeMeasure():
@@ -22,23 +25,26 @@ class TimeMeasure():
 
 time_measure = TimeMeasure()
 
+# GPI to Stream class with more information
 class GpiStream:
 	stream_id = '0'
 	in_cue = False
 	
 	def __init__(self, id):
-		self.gpi_input = gpi
 		self.stream_id = id
 	def __str__(self):
 		return "GPI: {} str_id: {} in_cue: {}".format(self.gpi_input, self.stream_id, self.in_cue)
 
+# Configure the web app (needed only for autorestar)
+app = Flask(__name__)
+app.config.from_object(cf.FlaskConfig)
 
+# Make a new dict with GPIs as Keys and GpiStreams as values
 gpi_stream_dict = {}
 for gpi, id in cf.gpi2stream.items():
 	gpi_stream_dict[gpi] = GpiStream(id)
 
 
-print(gpi_stream_dict[21].in_cue)
 # Setup GPIO inputs/outputs
 	#Use Board pin numbering - etc. (12) in pinout command
 GPIO.setmode(GPIO.BCM)
@@ -51,40 +57,39 @@ for GPI in list(cf.gpi2stream):
 
 # Start cue on Falling edge and Stop Cue on Rising edge
 def start_stop_avail(gpi):
-	print("4. Event detcted")
-	print("5. Stream is in cue: {}".format(gpi_stream_dict[gpi].in_cue))
+	edge = GPIO.input(gpi)
+	print("1. {} Event detcted".format(edge))
+	print("2. Stream is in cue: {}".format(gpi_stream_dict[gpi].in_cue))
 	
 	# Rising edge detected and Stream is in Cue => Stop cue
-	if GPIO.input(gpi) and gpi_stream_dict[gpi].in_cue:	
-		print("5. Stopping cue")
+	if edge and gpi_stream_dict[gpi].in_cue:	
+		print("3. Stopping cue")
 		time_measure.start_measure()
 		resp = liveapi.cue_command(cf.elemental_ip, cf.gpi2stream[gpi], 'stop_cue') # Stop cue
 		time_measure.end_measure()
 		#print(resp)
 		gpi_stream_dict[gpi].in_cue = False 		# Stream is no longer in cue
-		#time.sleep(cf.wait_time)					# Sleeps the thread for all GPIO inputs - not good
+		time.sleep(cf.wait_time)					# Sleeps the thread for all GPIO inputs - not good
 		
 	# Falling edge detected and Stream is NOT in Cue => Start cue
-	elif not GPIO.input(gpi) and not gpi_stream_dict[gpi].in_cue:	
-		print("5. Starting cue")
+	elif not edge and not gpi_stream_dict[gpi].in_cue:	
+		print("3. Starting cue")
 		time_measure.start_measure()
 		resp = liveapi.cue_command(cf.elemental_ip, cf.gpi2stream[gpi], 'start_cue') # Start cue			
 		time_measure.end_measure()
 		#print(resp)
 		gpi_stream_dict[gpi].in_cue = True			# Stream is now in cue
-		#time.sleep(cf.wait_time)					# Sleeps the thread for all GPIO inputs - not good
+		time.sleep(cf.wait_time)					# Sleeps the thread for all GPIO inputs - not good
 		
 
 # Tie callbacks to events
 for GPI in list(cf.gpi2stream):
-	GPIO.add_event_detect( GPI, GPIO.BOTH, callback = start_stop_avail, bouncetime = cf.wait_time*1000)
+	#GPIO.add_event_detect( GPI, GPIO.BOTH, callback = start_stop_avail, bouncetime = cf.wait_time*1000)
+	GPIO.add_event_detect( GPI, GPIO.BOTH, callback = start_stop_avail, bouncetime = 200)
 
-try:
-	print("3. Waiting for signal")
-	while 1:
-		pass
-		
-except KeyboardInterrupt:
-	pass
-GPIO.cleanup()
-		
+@app.route('/')
+def index():
+	return "Working"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')		
